8d866f47e5313542f5c904985d2f2372f9f23b2f
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-10-20 18:45:56 +0530

    Redux Implimentation start

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import * as serviceWorker from "./serviceWorker";
 
 // Redux
 import { Provider } from "react-redux";
+import store from "./redux/store"
 
 ReactDOM.render(
-    <App />,
-  document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
